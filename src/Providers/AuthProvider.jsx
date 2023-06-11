8a33944c7ff0d2a578f3ac25e0885638dac3c507
Shakilbhuyan <sbhuiyan2956@gmail.com>
2023-06-11 21:45:05 +0600

    Axios complete

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -3,6 +3,7 @@ import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStat
 import app from '../firebase/firebase.config';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import axios from 'axios';
 
 
 export const AuthContext = createContext(null);
@@ -43,7 +44,17 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             // get and set Token
-               setLoading(false)
+            if(currentUser){
+                axios.post('http://localhost:5000/jwt', {email : currentUser.email})
+                .then(data =>{
+                    // console.log(data.data.token)
+                    localStorage.setItem('access-token', data.data.token)
+                    setLoading(false)
+                })
+            }
+            else{
+                localStorage.removeItem('access-token')
+            }
             
         })
         return () => {
