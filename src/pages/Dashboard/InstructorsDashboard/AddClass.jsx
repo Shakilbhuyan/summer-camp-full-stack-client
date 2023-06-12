e3467382bec4927c93b1148685db5ffcfb4f98e5
Shakilbhuyan <sbhuiyan2956@gmail.com>
2023-06-12 17:31:19 +0600

    assed class complete

--- src/pages/Dashboard/InstructorsDashboard/AddClass.jsx
+++ src/pages/Dashboard/InstructorsDashboard/AddClass.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import { AuthContext } from '../../../Providers/AuthProvider';
 import { useContext } from 'react';
-import { useForm } from "react-hook-form";
 import Swal from 'sweetalert2';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const AddClass = () => {
     const { user } = useContext(AuthContext);
-    const { register, handleSubmit,reset } = useForm();
     const [axiosSecure] = useAxiosSecure();
 
-    const onSubmit = data => {
-          console.log(data);
+    const onSubmit = event => {
+        event.preventDefault();
+         const form = event.target;
+         const name = form.name.value;
+         const classimg = form.classimg.value;
+         const seats = form.seats.value;
+         const price = form.price.value;
+         
 
         const newItem = {
-            language: data.name,
-            image: data.classimg,
-            seatsAvailable: data.seats,
-            price: data.price,
+            language: name,
+            image: classimg,
+            seatsAvailable: seats,
+            price: price,
             email: user?.email,
             instructor: user?.displayName,
             instructorImg: user?.photoURL
         };
-
+    //  console.log(newItem)
         axiosSecure.post('/newclass', newItem)
             .then(res => {
                 if (res.data.insertedId) {
@@ -37,47 +41,49 @@ const AddClass = () => {
                 }
             })
     }
+
+   
     return (
         <>
             <h1 className='text-center my-4 text-3xl text-blue-700 font-bold'>Add class</h1>
             <div className="hero  bg-base-200">
                 <div className="card w-3/4 shadow-2xl bg-base-100">
-                    <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+                    <form onSubmit={onSubmit} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Class Name</span>
                             </label>
-                            <input type="text" placeholder="Name"  {...register("name", { required: true })} className="input input-bordered" />
+                            <input type="text" placeholder="Name"  name='name' className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Class Image</span>
                             </label>
-                            <input type="text"  {...register("classimg", { required: true })} placeholder="ImageURL" className="input input-bordered" />
+                            <input type="text"  name='classimg' placeholder="ImageURL" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Instuctor Name</span>
                             </label>
-                            <input type="text"  {...register("instructor", { required: true })} disabled defaultValue={user?.displayName} className="input input-bordered" />
+                            <input type="text"   disabled defaultValue={user?.displayName} className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" {...register("email", { required: true })} disabled defaultValue={user?.email} className="input input-bordered" />
+                            <input type="text"  disabled defaultValue={user?.email} className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Seats</span>
                             </label>
-                            <input type="number" {...register("seats", { required: true })} placeholder="seats" className="input input-bordered" />
+                            <input type="number" name='seats' placeholder="seats" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Price</span>
                             </label>
-                            <input type="number" {...register("price", { required: true })} placeholder="price" className="input input-bordered" />
+                            <input type="number" name='price' placeholder="price" className="input input-bordered" />
                         </div>
                         <input className='btn btn-primary' type="submit" value="Add Class" />
                     </form>
