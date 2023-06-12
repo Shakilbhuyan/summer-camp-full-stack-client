6442a50e20a7145b2544afc2dd1ccb7dfcc26ed3
Shakilbhuyan <sbhuiyan2956@gmail.com>
2023-06-12 16:52:07 +0600

    delete cart compelete

--- src/pages/Dashboard/StudentsDashboard/MyCart.jsx
+++ src/pages/Dashboard/StudentsDashboard/MyCart.jsx
@@ -2,10 +2,39 @@ import React from 'react';
 import { useCart } from '../../../hooks/useCart';
 import { Link } from 'react-router-dom';
 import { FaTrash } from 'react-icons/fa';
+import Swal from 'sweetalert2';
 
 const MyCart = () => {
     const [cart, refetch] = useCart();
     const total = cart.reduce((sum, item) => item.price + sum, 0).toFixed(2);
+
+    const handleDelete = id => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/carts/${id}`, {
+                    method: 'DELETE',
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0){
+                            refetch();
+                            Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+            }
+        })}
     return (
         <div className='w-3/4 my-8 '>
 
